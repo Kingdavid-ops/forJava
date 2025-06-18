73896cc1f1123800cade8d9d9d08d3da3325732b
Kingdavid-ops <kingdavidifediorah245@gmail.com>
2025-06-18 15:07:04 +0100

    simple calculations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int name = 29;
+        int sound = name*29;
+
+        System.out.println(sound);
         System.out.println("Hello, World!");
     }
 }
